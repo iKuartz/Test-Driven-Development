520e74e6495cf3199c420200effa16c63f59cc6b
Ivan Silva <ivankuartz@hotmail.com>
2022-05-19 12:00:15 -0300

    Added the test for the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,4 +7,8 @@ describe Solver do
   it 'Return the factorial of given number' do
     expect(@solver.factorial(5)).to eq(120)
   end
+  
+  it 'Reverses a string correctly' do
+    expect(@solver.reverse('hello')).to eq('olleh')
+  end
 end
