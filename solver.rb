0363357429fe94782764a9b352b8d7b79c4ce909
Ivan Silva <ivankuartz@hotmail.com>
2022-05-19 12:19:13 -0300

    Added implementation of fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -12,4 +12,22 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(number)
+    string = ''
+    if number%3 == 0
+      string = 'fizz'
+      if number%5 == 0
+        string = string + 'buzz'
+        return string
+      else
+        return string
+      end
+    elsif number%5 == 0
+      string = 'buzz'
+      return string
+    else
+      return number.to_s
+    end
+  end
 end
