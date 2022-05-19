c1ead5e5a3e98082d9f43b5f414884ca7a049171
Ivan Silva <ivankuartz@hotmail.com>
2022-05-19 12:07:24 -0300

    Added tests for Fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,4 +11,16 @@ describe Solver do
   it 'Reverses a string correctly' do
     expect(@solver.reverse('hello')).to eq('olleh')
   end
+
+  it 'Takes an integer N, checks if it is divisible by 3' do
+    expect(@solver.fizzbuzz(9)).to eq('fizz')
+  end
+
+  it 'Takes an integer N, checks if it is divisible by 5' do
+    expect(@solver.fizzbuzz(10)).to eq('buzz')
+  end
+
+  it 'Takes an integer N, checks if it is divisible by 3 and 5' do
+    expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+  end
 end
